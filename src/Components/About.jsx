8c0fa49fd71ccc6f3513ea8ef792fda5a5d9b40e
Tushar Arora <110760245+TUSHARARORATA@users.noreply.github.com>
2024-02-24 13:28:30 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "Software Developer background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm Tushar Arora GDSC'23 Lead "
+  "I'm Tushar Arora  Microsoft Learn Student Ambassador and Google DSC'23 Lead "
 
 /**
  * List of some of skills or technologies you work on, are learning,
