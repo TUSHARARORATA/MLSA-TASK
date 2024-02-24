7f462f125c98c6dbfef0e6f5987baa2ec16380f8
Tushar Arora <110760245+TUSHARARORATA@users.noreply.github.com>
2024-02-24 13:22:47 +0530

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/sd2.jpeg";
+import image from "../images/sd2.jpg";
 
 const imageAltText = "Software Developer";
 
