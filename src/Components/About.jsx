ef0143a52901650d2eb0a574763e8a7e3538d729
Tushar Arora <110760245+TUSHARARORATA@users.noreply.github.com>
2024-02-24 13:35:58 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/sd1.jpg";
+import image from "../images/sd2.jpg";
 
 const imageAltText = "Software Developer background";
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/SD4.jpg";
+import image from "../images/sd1.jpg";
 
 const imageAltText = "Tushar Arora - Full Stack MERN Developer . This is a background photo of Tushar Arora ";
 
